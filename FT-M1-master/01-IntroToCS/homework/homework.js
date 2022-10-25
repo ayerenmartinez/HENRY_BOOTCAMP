de5b59f3386ca1f0a2800b05a091dda765fd6f6f
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2022-10-25 16:44:16 -0500

    homework 01 IntroCs finished

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,33 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let dig=0;
+  let decimal=0;
+  let tamanio=0;
+  let pos=0;
+  tamanio = num.length;
+  console.log(tamanio);
+  for(let i=tamanio-1;i>=0;i--){
+      dig = num[pos];
+      decimal += dig*(Math.pow(2,i));
+      pos++;
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let dig=0;
+  let expo=0;
+  let binario=0;
+  let base=1;
+  while(num>0){
+       dig = Math.floor(num%2);
+       binario=  binario + dig*base;
+       num = Math.floor(num/2);   
+       base = base*10;
+  }
+  return binario.toString();
 }
 
 
